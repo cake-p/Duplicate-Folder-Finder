a2c02c9195311b2f44877267755e104d146352fe
Cake <psychoholic14@gmail.com>
2020-11-05 13:27:19 +0300

    Fixed big files, disk path, exclude files that cannot be read

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import argparse
 import hashlib
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 
 if cpu_count() > 1:
@@ -14,32 +15,37 @@ else:
     thread_count = 1
 
 pool = Pool(thread_count)
-folders_dict = {}
-files_dict = {}
-directory = ''
+FOLDERS_DICT = {}
+FILES_DICT = {}
+PATH = ''
+
+
+def _fill(path, folders, files, len_path, connector='\\'):
+    for folder_name in folders:
+        FOLDERS_DICT[(path + connector + folder_name)[len_path:]] = Folder()
+    for file_name in files:
+        FILES_DICT[(path + connector + file_name)[len_path:]] = File()
 
 
 # Сканирование папок для дальнейшего хэширования файлов и папок
 def scanning():
-    if directory[-1] == ':':
-        temp = directory + '\\'
+    len_path = len(PATH) + 1
+    if PATH[-1] == ':':
+        data_map = walk(PATH + '\\')
+        _fill(*next(data_map), len_path, '')
     else:
-        temp = directory
-    len_directory = len(directory) + 1
-    for dir, dirs, files in walk(temp):
-        for dir_name in dirs:
-            folders_dict[(dir + '\\' + dir_name)[len_directory:]] = Folder()
-        for file_name in files:
-            files_dict[(dir + '\\' + file_name)[len_directory:]] = File()
+        data_map = walk(PATH)
+    for path, folders, files in data_map:
+        _fill(path, folders, files, len_path)
 
 
 # Создание хэшей
 def generation_hashes(format):
     if format == 'normal':
         print('Создание хэша файлов')
-        results = tqdm(pool.imap_unordered(get_file_info, files_dict.keys()), total=len(files_dict))
+        results = tqdm(pool.imap_unordered(get_file_info, FILES_DICT.keys()), total=len(FILES_DICT), ascii=True)
     else:
-        results = pool.imap_unordered(get_file_info, files_dict.keys())
+        results = pool.imap_unordered(get_file_info, FILES_DICT.keys())
 
     for key in results:
         key_rspilt = key.rsplit('\\',1)
@@ -50,59 +56,85 @@ def generation_hashes(format):
     if format == 'normal':
         print('Создание хэша папок')
 
-    for key, info in reversed(folders_dict.items()):
-        dir_name = key.rsplit('\\',1)[0]
-        if dir_name != '':
-            update_folder_hash(dir_name, info)
+    for key, info in reversed(FOLDERS_DICT.items()):
+        path = key.rsplit('\\',1)[0]
+        if path != '':
+            update_folder_hash(path, info)
 
 
 # Получение информации о файле (размер и хэш)
 def get_file_info(key):
     try:
-        with open(directory+'\\'+key, 'rb') as f:
-            files_dict[key].sha512 = hashlib.sha512(f.read()).digest()
-            files_dict[key].size = fstat(f.fileno()).st_size
+        with open(PATH+'\\'+key, 'rb') as f:
+            size = fstat(f.fileno()).st_size
+            chunk_size = 536870912
+            if size > chunk_size:
+                temp = b''
+                hash = False
+                if len(key) > 50:
+                    desc = '..' + key[-48:]
+                else:
+                    desc = key
+                tbar = tqdm(desc=desc, total=int(size/1024/1024), unit='MB', leave=False, ascii=True)
+                with ThreadPoolExecutor(max_workers=2) as executor:
+                    while True:
+                        data = executor.submit(f.read, chunk_size).result()
+                        if hash:
+                            temp += hash.result().digest()
+                            tbar.update(int(chunk_size/1024/1024))
+                        if not data:
+                            break
+                        hash = executor.submit(hashlib.sha512, data)
+                FILES_DICT[key].sha512 = hashlib.sha512(temp).digest()
+                tbar.close()
+            else:
+                FILES_DICT[key].sha512 = hashlib.sha512(f.read()).digest()
+            FILES_DICT[key].size = size
     except OSError as e:
-        if e.errno == 13:
-            files_dict[key].sha512 = hashlib.sha512(key.rsplit('\\',1)[-1].encode()).digest()
-            files_dict[key].size = len(key.rsplit('\\',1)[-1])
+        # При невозможности считать файл используется не содержимое файла, а его имя
+        if e.errno == 13 or e.errno == 22 or e.errno == 2:
+            FILES_DICT[key].sha512 = hashlib.sha512(key.rsplit('\\',1)[-1].encode()).digest()
+            FILES_DICT[key].size = len(key.rsplit('\\',1)[-1])
         else:
             raise e
     return key
 
 
 # Обновление данных папки
-def update_folder_info(key, dir_name):
-    folders_dict[dir_name].size += files_dict[key].size
-    folders_dict[dir_name].hash.append(files_dict[key].sha512)
+def update_folder_info(key, path):
+    FOLDERS_DICT[path].size += FILES_DICT[key].size
+    FOLDERS_DICT[path].hash.append(FILES_DICT[key].sha512)
 
 
-def update_folder_hash(dir_name, info):
+def update_folder_hash(path, info):
     info.hash.sort()
-    folders_dict[dir_name].hash.append(hashlib.sha512(b''.join(info.hash)).digest())
-    folders_dict[dir_name].size += info.size
+    FOLDERS_DICT[path].hash.append(hashlib.sha512(b''.join(info.hash)).digest())
+    FOLDERS_DICT[path].size += info.size
 
 
 def update_folder_hash_to_sha512():
-    for key, info in folders_dict.items():
-        if folders_dict[key].sha512 != b'':
+    for key, info in FOLDERS_DICT.items():
+        if FOLDERS_DICT[key].sha512 != b'':
             continue
         info.hash.sort()
-        folders_dict[key].sha512 = hashlib.sha512(b''.join(info.hash)).digest()
+        FOLDERS_DICT[key].sha512 = hashlib.sha512(b''.join(info.hash)).digest()
 
 
 # Сравнение папок по хэшу и размеру
-def finder(format):
+def finder(format, out):
     temp = {}
     sha512size_list = []
-    for dir_name, info in folders_dict.items():
+    for path, info in FOLDERS_DICT.items():
         key = info.sha512.hex()+'-'+str(info.size)
         if not key in temp:
             temp[key] = []
-        temp[key].append(directory[4:]+dir_name)
+        temp[key].append(PATH[4:]+'\\'+path)
     # Исправить производительность цикла
-    for key, temp_data in temp.items():
+    for key, temp_data in tqdm(temp.items(), total=len(temp), ascii=True):
         if len(temp_data) > 1:
+            size = int(key.split('-')[1])
+            if size == 0:
+                continue
             folders = tuple(temp_data)
             i = 0
             for data in sha512size_list:
@@ -114,12 +146,12 @@ def finder(format):
                             i += 1
             if len(folders) != i:
                 sha512size_list.append({
-                    'size': int(key.split('-')[1]),
+                    'size': size,
                     'folders': temp_data,
                 })
     sha512size_list = sorted(sha512size_list, key=lambda i: i['size'], reverse=True)
     data = json.dumps(sha512size_list)
-    with open('data.json', 'w') as f:
+    with open(out, 'w') as f:
         f.write(data)
 
     if format == 'json':
@@ -156,11 +188,16 @@ if __name__ == "__main__":
         type=str,
         default='normal',
     )
+    parser.add_argument(
+        '--out',
+        type=str,
+        default='data.json',
+    )
     FLAGS, unparsed = parser.parse_known_args()
     if FLAGS.dir[-1] in ('\\', '/'):
-        directory = '\\\\?\\' + FLAGS.dir[:-1]
+        PATH = '\\\\?\\' + FLAGS.dir[:-1]
     else:
-        directory = '\\\\?\\' + FLAGS.dir
+        PATH = '\\\\?\\' + FLAGS.dir
 
     if FLAGS.format == 'normal':
         print('Сканирование: ' + FLAGS.dir)
@@ -170,4 +207,4 @@ if __name__ == "__main__":
 
     if FLAGS.format == 'normal':
         print('Поиск дубликатов')
-    finder(FLAGS.format)
+    finder(FLAGS.format, FLAGS.out)
